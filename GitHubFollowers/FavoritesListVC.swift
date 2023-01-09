e5c39c39583e2752766a8af8d7e5433385edd658
Mila B <milabochkova03@gmail.com>
2023-01-09 08:52:16 +0300

    initial commit

---
+++ GitHubFollowers/FavoritesListVC.swift
@@ -0,0 +1,18 @@
+//
+//  FavoritesListVC.swift
+//  GitHubFollowers
+//
+//  Created by Mila B on 09.01.2023.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+
+
+}
---
+++ GitHubFollowers/SearchVC.swift
@@ -0,0 +1,17 @@
+//
+//  SearchVC.swift
+//  GitHubFollowers
+//
+//  Created by Mila B on 09.01.2023.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+
+}
