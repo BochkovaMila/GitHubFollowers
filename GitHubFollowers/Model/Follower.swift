b7140f732591a65edde2ccac1a00219648d739b2
Mila B <milabochkova03@gmail.com>
2023-01-10 13:33:27 +0300

    add NetworkManager

---
+++ GitHubFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GitHubFollowers
+//
+//  Created by Mila B on 10.01.2023.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
