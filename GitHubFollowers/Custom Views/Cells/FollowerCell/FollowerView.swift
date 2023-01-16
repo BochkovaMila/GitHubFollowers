c25330250104c5659010ccd99b2a9f8b88fbe1b7
Mila B <milabochkova03@gmail.com>
2023-01-16 16:25:10 +0300

    iOS 16 Project Update

---
+++ GitHubFollowers/Custom Views/Cells/FollowerCell/FollowerView.swift
@@ -0,0 +1,37 @@
+//
+//  FollowerView.swift
+//  GitHubFollowers
+//
+//  Created by Mila B on 16.01.2023.
+//
+
+import SwiftUI
+
+struct FollowerView: View {
+    
+    var follower: Follower
+    
+    var body: some View {
+        VStack {
+            AsyncImage(url: URL(string: follower.avatarUrl)) { image in
+                image
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+            } placeholder: {
+                Image("avatar-placeholder")
+            }
+            .clipShape(Circle())
+            
+            Text(follower.login)
+                .bold()
+                .lineLimit(1)
+                .minimumScaleFactor(0.6)
+        }
+    }
+}
+
+struct FollowerView_Previews: PreviewProvider {
+    static var previews: some View {
+        FollowerView(follower: Follower(login: "SeanAllen", avatarUrl: ""))
+    }
+}
